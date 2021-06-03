de7abde7e9c6b6b75cab6e4d723018c297b1e563
Nishan Bajracharya <nishanbajracharya@lftechnology.com>
2021-06-03 13:59:40 +0545

    Add initialize and cleanup functions

--- src/js/auto.js
+++ src/js/auto.js
@@ -2,10 +2,31 @@ import 'uswds/src/js/polyfills';
 import domready from 'domready';
 import * as components from './components';
 
-domready(() => {
+function initialize() {
   const target = document.body;
   Object.keys(components).forEach((key) => {
     const behavior = components[key];
     behavior.on(target);
   });
-});
+}
+
+function cleanup() {
+  Object.keys(components).forEach((key) => {
+    const behavior = components[key];
+    behavior.off();
+  });
+}
+
+function reinitialize() {
+  cleanup();
+  initialize();
+}
+
+domready(initialize);
+
+window.uswds = {
+  cleanup,
+  initialize,
+  components,
+  reinitialize,
+};
